b064002b9597a881c556ba0b2b6f10e4d5179cf4
Denguiros <fehmi.denguir@enis.tn>
2021-10-20 00:25:23 +0100

    Added service and controller

--- src/main/java/com/example/demo/dao/PublicationRepository.java
+++ src/main/java/com/example/demo/dao/PublicationRepository.java
@@ -4,15 +4,15 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 
 import com.example.demo.entities.Publication;
 
-@Repository
+@RepositoryRestController
 public interface PublicationRepository extends JpaRepository<Publication, Long> {
 	List<Publication> findByDate(Date date);
 	List<Publication> findByType(String type);
-	Publication findBySourcePDF(String sourcePDF);
+	List<Publication> findBySourcePDF(String sourcePDF);
 	Publication findByTitre(String titre);
 	Publication findByLien(String lien);
 }
